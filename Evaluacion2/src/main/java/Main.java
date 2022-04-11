593727ef2e1c8fb64d1850edb8eb6ea7de03f6f8
sa_fi <sa_fi@DESKTOP-7I7EMK3>
2022-04-11 01:13:30 -0400

    commit main

--- Evaluacion2/src/main/java/Main.java
+++ Evaluacion2/src/main/java/Main.java
@@ -3,8 +3,7 @@ import com.evaluacion2.conexion.Conexion;
 public class Main {
 
 	public static void main(String[] args) {
-		Conexion con = new Conexion();
-    	System.out.println("exito3" + con);
+		Conexion con = new Conexion();;
 
 	}
 
